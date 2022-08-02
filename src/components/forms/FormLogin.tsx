ac6760b88043cb4f09e7cf1f5e84dcabe26d1fce
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-02 15:54:01 -0300

    fix: remove logic from FormLogin

--- src/components/forms/FormLogin.tsx
+++ src/components/forms/FormLogin.tsx
@@ -1,15 +1,6 @@
 import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
-import {
-  isSignInWithEmailLink,
-  sendSignInLinkToEmail,
-  signInWithEmailLink,
-} from 'firebase/auth';
-import { useEffect } from 'react';
 import Card from '@/components/core/Card';
-import { actionCodeSettings } from '@/firebase/auth';
-import { auth } from '@/firebase';
-import tryCatch from '@/helpers/tryCatch';
 import Text from '@/components/core/Text';
 import FormikInput from '@/components/forms/FormikInput';
 import Button from '@/components/core/Button';
@@ -31,42 +22,8 @@ const FormLogin = ({ title }: Props) => {
   });
 
   const handleSubmit = async ({ email }: typeof initialValues) => {
-    const [, error] = await tryCatch(
-      sendSignInLinkToEmail(auth, email, actionCodeSettings)
-    );
-    console.log(actionCodeSettings);
-    if (error) {
-      alert('Falha ao enviar email. Favor tente fazer o login novamente');
-    }
-
-    window.localStorage.setItem('comerciio-sign-in-email', email);
+    console.log(email);
   };
-
-  const verifyIfEmailWasSent = () => {
-    if (isSignInWithEmailLink(auth, window.location.href)) {
-      let email = window.localStorage.getItem('comerciio-sign-in-email');
-
-      if (!email) {
-        email = window.prompt(
-          'Insira o email novamente para confirmarmos sua identidade'
-        );
-      }
-
-      signInWithEmailLink(auth, email || '', window.location.href)
-        .then(() => {
-          window.localStorage.removeItem('comerciio-sign-in-email');
-        })
-        .catch(() => {
-          alert(
-            'Seu email pode estar diferente do email digitado anteriormente ou seu link expirou. Favor tente fazer o login novamente'
-          );
-        });
-    }
-  };
-
-  useEffect(() => {
-    verifyIfEmailWasSent();
-  }, []);
 
   return (
     <Card shadow className="w-5/6 max-w-sm">
