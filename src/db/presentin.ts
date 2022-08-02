56f5ebc4748dfb7fc738b11a39be2ea44eb109b7
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-02 16:00:17 -0300

    feat: setup presentin crud

---
+++ src/db/presentin.ts
@@ -0,0 +1,31 @@
+import { adminFirestore, getServerTimestamp } from '@/services/firebase/admin';
+import { Presentin } from '@/types/presentin';
+
+export const addPresentin = (data: Presentin) =>
+  adminFirestore.collection('presentin').add(data);
+
+export const getPresentinById = (id: string) =>
+  adminFirestore.collection('presentin').doc(id).get();
+
+export const updatePresentinById = (
+  id: string,
+  uid: string,
+  data: Presentin
+) => {
+  const presentinRef = adminFirestore.collection('presentin').doc(id);
+
+  return adminFirestore.runTransaction(async (transaction) => {
+    const presentinDoc = await transaction.get(presentinRef);
+    const presentin = presentinDoc.data();
+
+    if (!presentin) throw new Error('Presentin not found');
+
+    if (uid !== presentin.uid)
+      throw new Error('You cannot edit this presentin');
+
+    transaction.update(presentinRef, {
+      ...data,
+      updatedAt: getServerTimestamp(),
+    });
+  });
+};
