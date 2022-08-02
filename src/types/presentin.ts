39e757e9f11a69fec1a25044a76ba12442272446
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-02 16:00:38 -0300

    fix: add missing fields to presentin type

--- src/types/presentin.ts
+++ src/types/presentin.ts
@@ -1,7 +1,10 @@
 export interface Presentin {
   id: string;
+  uid: string;
   recipientName: string;
   title: string;
   collectCash: boolean;
   groupName: string;
+  createdAt: string;
+  updatedAt: string;
 }
