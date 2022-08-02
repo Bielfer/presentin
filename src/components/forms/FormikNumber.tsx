8a473b2bd65b716c8a0c66877eceed8a9653148e
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-02 15:52:14 -0300

    fix: FormikNumber should return a number

--- src/components/forms/FormikNumber.tsx
+++ src/components/forms/FormikNumber.tsx
@@ -1,20 +1,42 @@
 import { useField } from 'formik';
-import { InputHTMLAttributes } from 'react';
+import { ChangeEvent, InputHTMLAttributes, useState } from 'react';
 import Input from '../core/Input';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   name: string;
   hint?: string;
+  format?: 'default' | 'currency';
 }
 
-const FormikNumber = ({ name, label, hint, className, ...props }: Props) => {
-  const [{ value }, { error }, { setValue }] = useField(name);
+const FormikNumber = ({
+  name,
+  label,
+  hint,
+  className,
+  format = 'default',
+  ...props
+}: Props) => {
+  const [, { error, initialValue }, { setValue }] = useField(name);
+  const [input, setInput] = useState(initialValue);
+
+  const formats = {
+    default: (value: string) => Number(value.replace(',', '.')),
+    currency: (value: string) =>
+      Math.floor(Number(value.replace(',', '.')) * 100),
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    setInput(value);
+    setValue(formats[format](value));
+  };
 
   return (
     <Input
-      value={value}
-      onChange={(e) => setValue(e.target.value.replace(',', '.'))}
+      value={input}
+      onChange={handleChange}
       label={label}
       className={className}
       error={error}
