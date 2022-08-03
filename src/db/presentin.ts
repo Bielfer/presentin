8e974adb2bf0bc9ccc3c149544f19dce873d319e
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-03 00:31:21 -0300

    feat: presentin message,presentins collection name

--- src/db/presentin.ts
+++ src/db/presentin.ts
@@ -1,21 +1,21 @@
 import { adminFirestore, getServerTimestamp } from '@/services/firebase/admin';
-import { Presentin } from '@/types/presentin';
+import { Presentin, PresentinMessage } from '@/types/presentin';
 
 export const addPresentin = (data: Presentin) =>
-  adminFirestore.collection('presentin').add({
+  adminFirestore.collection('presentins').add({
     ...data,
     updatedAt: getServerTimestamp(),
   });
 
 export const getPresentinById = (id: string) =>
-  adminFirestore.collection('presentin').doc(id).get();
+  adminFirestore.collection('presentins').doc(id).get();
 
 export const updatePresentinById = (
   id: string,
   uid: string,
   data: Presentin
 ) => {
-  const presentinRef = adminFirestore.collection('presentin').doc(id);
+  const presentinRef = adminFirestore.collection('presentins').doc(id);
 
   return adminFirestore.runTransaction(async (transaction) => {
     const presentinDoc = await transaction.get(presentinRef);
@@ -30,5 +30,46 @@ export const updatePresentinById = (
       ...data,
       updatedAt: getServerTimestamp(),
     });
+  });
+};
+
+export const getPresentinMessages = (presentinId: string) =>
+  adminFirestore
+    .collection('presentins')
+    .doc(presentinId)
+    .collection('messages')
+    .get();
+
+export const addPresentinMessage = (
+  presentinId: string,
+  data: PresentinMessage
+) =>
+  adminFirestore
+    .collection('presentins')
+    .doc(presentinId)
+    .collection('messages')
+    .add({ ...data, updatedAt: getServerTimestamp() });
+
+export const deletePresentinMessage = (
+  uid: string,
+  presentinId: string,
+  messageId: string
+) => {
+  const presentinRef = adminFirestore.collection('presentins').doc(presentinId);
+  const messageRef = presentinRef.collection('messages').doc(messageId);
+
+  return adminFirestore.runTransaction(async (transaction) => {
+    const presentinDoc = presentinRef.get();
+    const presentin = (await presentinDoc).data();
+    const messageDoc = messageRef.get();
+    const message = (await messageDoc).data();
+
+    if (!message) throw new Error('Message not found');
+    if (!presentin) throw new Error('Presentin not found');
+
+    if (message.uid !== uid && presentin.uid !== uid)
+      throw new Error('You cannot delete this message');
+
+    transaction.delete(messageRef);
   });
 };
