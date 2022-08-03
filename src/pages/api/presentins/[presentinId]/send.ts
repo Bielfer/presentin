d95541cbd7b2479a23210f0f6f6b45ed02335d17
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-03 14:47:21 -0300

    feat: endpoint send presentin

---
+++ src/pages/api/presentins/[presentinId]/send.ts
@@ -0,0 +1,35 @@
+import { updatePresentinById } from '@/db/presentin';
+import { isAuthenticated, requestTimer } from '@/helpers/middlewares';
+import tryCatch from '@/helpers/tryCatch';
+import { NextApiRequestExtended } from '@/types/api';
+import { PresentinStatus } from '@/types/presentin';
+import type { NextApiResponse } from 'next';
+import { createRouter } from 'next-connect';
+
+const router = createRouter<NextApiRequestExtended, NextApiResponse>().use(
+  requestTimer
+);
+
+router.use(isAuthenticated).post(async (req, res) => {
+  const { presentinId } = req.query;
+  const uid = req.token?.uid;
+
+  const [, error] = await tryCatch(
+    updatePresentinById(presentinId as string, uid ?? '', {
+      status: PresentinStatus.Sent,
+    })
+  );
+
+  if (error) {
+    res.status(400).json({ message: 'Failed to send presentin', error });
+    return;
+  }
+
+  res.status(200).json({ message: 'Presentin sent successfully' });
+});
+
+export default router.handler({
+  onNoMatch: (req, res) => {
+    res.status(405).json({ message: 'Invalid Request Method' });
+  },
+});
