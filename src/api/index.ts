471081b5b26ef1ec7b4fa716bb2a2da14724d02b
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-04 22:22:46 -0300

    feat: setup apiWithToken handler

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,21 @@
+import { auth } from '@/services/firebase';
 import axios from 'axios';
 
 const api = axios;
+const apiWithToken = axios;
 
-export default api;
+api.defaults.baseURL = `${process.env.NEXT_PUBLIC_APP_URL}/api`;
+apiWithToken.defaults.baseURL = `${process.env.NEXT_PUBLIC_APP_URL}/api`;
+
+apiWithToken.interceptors.request.use(async (config) => {
+  const newConfig = { ...config };
+  const token = await auth.currentUser?.getIdToken();
+
+  if (!newConfig.headers) return null;
+
+  newConfig.headers.authorization = `Bearer ${token}`;
+
+  return newConfig;
+});
+
+export { api, apiWithToken };
