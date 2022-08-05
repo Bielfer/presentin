cc4660628a1acf44e3cb029828cf7621e1bb699b
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-04 22:59:40 -0300

    feat: presentin api requests

---
+++ src/api/presentin.ts
@@ -0,0 +1,21 @@
+import { ResponseFormat } from '@/types/api';
+import { Presentin } from '@/types/presentin';
+import { apiWithToken } from '.';
+
+const createPresentin = (body: Record<string, any>) =>
+  apiWithToken
+    .post<ResponseFormat<Presentin>>('/presentins', body)
+    .then((res) => res.data.data);
+
+const editPresentin = (presentinId: string, body: Record<string, any>) =>
+  apiWithToken.patch(`presentins/${presentinId}`, body).then((res) => res.data);
+
+const createPresentinMessage = (
+  presentinId: string,
+  body: Record<string, any>
+) =>
+  apiWithToken
+    .post(`/presentins/${presentinId}/message`, body)
+    .then((res) => res.data);
+
+export { createPresentin, editPresentin, createPresentinMessage };
