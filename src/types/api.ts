27661391faaa788b531ba356a2f63475e40688d1
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-04 22:43:03 -0300

    feat: add ResponseFormat type for axios

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,6 +1,11 @@
-import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
-import type { NextApiRequest } from "next";
+import { DecodedIdToken } from 'firebase-admin/lib/auth/token-verifier';
+import type { NextApiRequest } from 'next';
 
 export interface NextApiRequestExtended extends NextApiRequest {
   token: DecodedIdToken | null;
+}
+
+export interface ResponseFormat<T> {
+  message: string;
+  data: T;
 }
