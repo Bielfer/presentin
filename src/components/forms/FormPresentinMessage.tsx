3d197e86b56f254eb1c053536673eac2d1bd3aca
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-06 15:42:50 -0300

    feat: add message to presentin

--- src/components/forms/FormPresentinMessage.tsx
+++ src/components/forms/FormPresentinMessage.tsx
@@ -1,11 +1,18 @@
+import { createPresentinMessage } from '@/api/presentin';
+import { createUser } from '@/api/user';
 import hints from '@/constants/hints';
+import paths from '@/constants/paths';
 import validations from '@/constants/validations';
 import { useAuth } from '@/contexts/auth';
+import { uploadPresentinMessageImage } from '@/db/files';
+import tryCatch from '@/helpers/tryCatch';
 import { CameraIcon } from '@heroicons/react/solid';
 import clsx from 'clsx';
 import { Form, Formik } from 'formik';
+import { useRouter } from 'next/router';
 import * as Yup from 'yup';
 import Button from '../core/Button';
+import { useToast } from '../core/Toast';
 import FormikFile from './FormikFile';
 import FormikInput from './FormikInput';
 import FormikNumber from './FormikNumber';
@@ -17,7 +24,11 @@ interface Props {
 }
 
 const FormPresentinMessage = ({ className }: Props) => {
-  const { sendEmailLink } = useAuth();
+  const { addToast } = useToast();
+  const { signInAnonymous, loggedIn } = useAuth();
+  const router = useRouter();
+
+  const presentinId = router.query.presentinId as string;
 
   const initialValues = {
     senderName: '',
@@ -25,6 +36,8 @@ const FormPresentinMessage = ({ className }: Props) => {
     image: null,
     donateCash: false,
     cashAmount: 0,
+    loggedIn,
+    senderEmail: '',
   };
 
   const validationSchema = Yup.object({
@@ -37,11 +50,62 @@ const FormPresentinMessage = ({ className }: Props) => {
         .min(1, validations.valueGreaterThanZero)
         .required(validations.required),
     }),
+    senderEmail: Yup.string().when('loggedIn', {
+      is: false,
+      then: Yup.string().required(validations.required),
+    }),
   });
 
   const handleSubmit = async (values: typeof initialValues) => {
-    sendEmailLink('gfvf2001@gmail.com');
-    console.log(values);
+    if (!loggedIn) {
+      await signInAnonymous({ displayName: values.senderName });
+      const [, error] = await tryCatch(
+        createUser({ name: values.senderName, email: values.senderEmail })
+      );
+      if (error) {
+        addToast({
+          type: 'error',
+          content: 'Falha ao enviar mensagem, tente recarregar a página',
+        });
+      }
+    }
+
+    let downloadUrl: string | null = null;
+
+    if (values.image) {
+      const [url, error] = await tryCatch(
+        uploadPresentinMessageImage(values.image)
+      );
+
+      if (error)
+        addToast({
+          type: 'error',
+          content: 'Falha ao subir imagem, tente novamente',
+        });
+
+      downloadUrl = url;
+    }
+
+    const {
+      loggedIn: x,
+      senderEmail,
+      ...toSendValues
+    } = { ...values, image: downloadUrl };
+
+    const [presentinData, error] = await tryCatch(
+      createPresentinMessage(presentinId, toSendValues)
+    );
+
+    if (error || !presentinData) {
+      addToast({
+        type: 'error',
+        content:
+          'Não foi possível enviar a sua mensagem, tente recarregar a página',
+      });
+      return;
+    }
+
+    router.push(paths.presentinById(presentinId));
   };
 
   return (
@@ -82,6 +146,15 @@ const FormPresentinMessage = ({ className }: Props) => {
               label="Digite o valor"
               hint={hints.required}
               format="currency"
+            />
+          )}
+
+          {!loggedIn && (
+            <FormikInput
+              name="senderEmail"
+              label="Seu Email"
+              hint={hints.required}
+              placeholder="Ex: maria@meuemail.com"
             />
           )}
 
