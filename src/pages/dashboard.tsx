adea10bb42e2f74cd424a0f41740f2f393881983
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-06 18:11:07 -0300

    feat: dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,67 @@
+import ButtonLink from '@/components/core/ButtonLink';
+import Container from '@/components/core/Container';
+import Header from '@/components/core/Header';
+import LoadingWrapper from '@/components/core/LoadingWrapper';
+import MyLink from '@/components/core/MyLink';
+import Table from '@/components/core/Table';
+import Text from '@/components/core/Text';
+import paths from '@/constants/paths';
+import usePresentins from '@/data/usePresentins';
+import { Page } from '@/types/auth';
+
+const Dashboard: Page = () => {
+  const { presentins, loading } = usePresentins();
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className="flex items-center justify-between">
+          <Text h3>Seus Presentins</Text>
+          <ButtonLink href={paths.createPresentin} variant="primary">
+            Criar
+          </ButtonLink>
+        </div>
+        <LoadingWrapper loading={loading}>
+          {presentins?.length === 0 ? (
+            <Text className="justify-center text-xl my-8">
+              Você ainda não tem nenhum presentin, aproveite para criar agora!
+            </Text>
+          ) : (
+            <Table className="my-8">
+              <Table.Head>
+                <Table.Header>Título</Table.Header>
+                <Table.Header>Recebedor</Table.Header>
+                <Table.Header hidden>Editar</Table.Header>
+              </Table.Head>
+              <Table.Body>
+                {presentins?.map((presentin) => (
+                  <Table.Row key={presentin.id}>
+                    <Table.Data>
+                      <MyLink href={paths.presentinById(presentin.id)}>
+                        {presentin.title}
+                      </MyLink>
+                    </Table.Data>
+                    <Table.Data>{presentin.recipientName}</Table.Data>
+                    <Table.Data className="flex items-center justify-end">
+                      <ButtonLink
+                        href={paths.presentinById(presentin.id)}
+                        size="xs"
+                      >
+                        Editar
+                      </ButtonLink>
+                    </Table.Data>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
+          )}
+        </LoadingWrapper>
+      </Container>
+    </>
+  );
+};
+
+Dashboard.auth = 'block';
+
+export default Dashboard;
