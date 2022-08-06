a036040c06a473d4afeeb5b8b9b144c0ff368f8d
Gabriel Fernandes <bielfer18@gmail.com>
2022-08-06 00:16:54 -0300

    feat: presentin send and recipient view

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -7,6 +7,10 @@ const paths = {
   presentinById: (presentinId: string) => `/presentin/${presentinId}`,
   presentinMessage: (presentinId: string) =>
     `/presentin/${presentinId}/message`,
+  presentinRecipient: (presentinId: string) =>
+    `/presentin/${presentinId}/recipient`,
+  presentinSend: (presentinId: string) => `/presentin/${presentinId}/send`,
+  presentinExample: '/presentin/example',
 };
 
 export default paths;
